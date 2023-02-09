397147a4e10fdff8b7df7a0394a850bcfe15b1d8
bravebarry <wojciechowskimarcink@gmail.com>
2023-02-09 19:38:07 +0100

    Zadanie 5 versja 2

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,15 +1,17 @@
 ﻿Console.WriteLine("Podaj liczbę:");
 string number = Console.ReadLine();
+string numberInString = number.ToString();
+char[] letters = numberInString.ToArray();
 List<int> count = new List<int>(new int[10]);
 
-foreach (char digit in number)
+foreach (char digit in letters)
 {
     int num = digit - '0';
     count[num]++;
 }
 
 int i = 0;
-foreach (int value in count)
+foreach (int value in count.ToArray())
 {
     Console.WriteLine("Cyfra " + i + " występuje " + value + " razy");
     i++;
