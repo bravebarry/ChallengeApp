684ced2411d8492773b74a6118cb11cbce38a569
bravebarry <wojciechowskimarcink@gmail.com>
2023-02-09 20:42:45 +0100

    Zadanie 5 kolejna wersja w 12 linijkach kodu

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,21 +1,12 @@
 ﻿Console.WriteLine("Podaj liczbę:");
 string number = Console.ReadLine();
-string numberInString = number.ToString();
-char[] letters = numberInString.ToArray();
 List<int> count = new List<int>(new int[10]);
 
-foreach (char digit in letters)
+foreach (char digit in number)
 {
     int num = digit - '0';
     count[num]++;
 }
 
-int i = 0;
-foreach (int value in count.ToArray())
-{
-    Console.WriteLine("Cyfra " + i + " występuje " + value + " razy");
-    i++;
-}
-Console.ReadLine();
-
-// Zadanie dnia 5
+for (int i = 0; i < 10; i++)
+    Console.WriteLine("Cyfra " + i + " występuje " + count[i] + " razy");
