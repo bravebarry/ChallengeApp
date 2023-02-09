db9b511535a3824f1f5c703e843ef689e06a0a34
bravebarry <wojciechowskimarcink@gmail.com>
2023-02-09 15:13:19 +0100

    praca domowa dnia 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// praca domowa dnia 4
+namespace ChallengeApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Ewa";
+            string gender = "Kobieta";
+            int age = 33;
+
+            if (gender == "Kobieta" && age < 30)
+            {
+                Console.WriteLine("Kobieta poniżej 30 lat");
+            }
+            else if (name == "Ewa" && age == 33)
+            {
+                Console.WriteLine("Ewa, lat 33");
+            }
+            else if (gender == "Mężczyzna" && age < 18)
+            {
+                Console.WriteLine("Niepełnoletni Mężczyzna");
+            }
+        }
+    }
+}
